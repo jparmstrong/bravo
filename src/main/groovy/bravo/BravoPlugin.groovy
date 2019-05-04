afbe26034ec4be0631c9034d55a46d8c31a596b3
J.P. Armstrong <jp@armstrong.sh>
2019-05-04 17:19:05 -0400

    Add new install dep task

--- src/main/groovy/bravo/BravoPlugin.groovy
+++ src/main/groovy/bravo/BravoPlugin.groovy
@@ -1,19 +1,45 @@
+package bravo
+
+import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Copy
+
 
 // CREATE CUSTOM dataFiles DEPENDENCY - SEE https://guides.gradle.org/implementing-gradle-plugins/
 
 class BravoPlugin implements Plugin<Project> {
+
+    String installLoc = ""
+
     void apply(Project project) {
+
         project.task('depenv') {
             doLast {
-                println "Compile dependencies"
-                def selectedDeps = project.configurations.compile.incoming.resolutionResult.allDependencies.collect { dep ->
+                println "Generating dep.env"
+                def selectedDeps = project.configurations.external.incoming.resolutionResult.allDependencies.collect { dep ->
                     "${dep.selected}"
                 }
-                new File("$buildDir/dep.env").text = selectedDeps.unique().sort().join("\n")
-
+                new File("$project.projectDir/dep.env").text = selectedDeps.unique().sort().join("\n")
             }
         }
+
+        project.configurations {
+            external
+        }
+
+        project.task("install", type: Copy, overwrite: true) {
+            if (!project.hasProperty("install_loc")) {
+                throw new GradleException('-Pinstall_loc required')
+            }
+
+            this.installLoc = project.getProperty('install_loc')
+            println "Installing deps to " + this.installLoc
+
+            from project.configurations.external
+            into  this.installLoc
+        }
+
+        project.build.dependsOn  'depenv' // 'copyDependencies',
     }
 }
